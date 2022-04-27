b7d939a9ce30a3dcaf000fba80079e2f6e23ba38
TrvMarco <trevisanello.marco@gmail.com>
2022-04-27 18:04:56 +0200

    fix

--- js/script.js
+++ js/script.js
@@ -5,6 +5,4 @@ const cognome = prompt('Inserisci il tuo Cognome ');
 // CHIEDERE ALL'UTENTE IL SUO COLORE PREFERITO
 const colore = prompt('Inserisci il tuo colore preferito');
 // GENERARE LA PASSWORD UTENTE 
-document.getElementById('passwordgenerata').innerHTML = (
-    `${nome}${cognome}${colore}${21}`
-);
+document.getElementById('passwordgenerata').innerHTML = `${nome}${cognome}${colore}21`;
